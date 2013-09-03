e72bebdd5901c5e9b1d2a5f68c1a8aad777196c0
slashedtires <kenneth.m.piekarski@gmail.com>
2013-09-02 22:02:02 -0700

    sorting built ranks

--- lib/rank_list.rb
+++ lib/rank_list.rb
@@ -1,11 +1,12 @@
 class RankList
 	def initialize(team_ranks)
-		@team_ranks = team_ranks
+		@team_names_and_ranks = team_ranks
 	end
 
 	def build_ranks
-		@team_ranks.sort_by { |team_name, team_rank| [team_rank, team_name] }
-		@team_ranks.sort_by {|team_rank| team_rank}.reverse
+		@team_name_and_ranks.sort_by do |team_name, team_rank|
+			[-Integer(team_rank), team_name] 
+		end
 	end
 
 #	def output_ranks
